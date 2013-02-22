7f54f609540f905cef0448bb7698129552e33b70
Tom Burns <tom.burns@jadedpixel.com>
2013-02-22 14:18:26 -0500

    initial commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CapacityMonitor::Application.config.secret_token = '5cbbae8341cf50df1f1946a67fd3da5ba9dfcdcadf660db4161f1d8f1ef370da74149fc26ab9d3332083e480ab2c8978e388001c2758015c062b23d29f024eb3'
