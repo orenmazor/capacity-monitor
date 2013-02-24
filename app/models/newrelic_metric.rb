528886b72978227d75514390a2355f7da2831157
Tom Burns <tom.burns@jadedpixel.com>
2013-02-24 16:52:00 -0500

    generate samples for each metric

--- app/models/newrelic_metric.rb
+++ app/models/newrelic_metric.rb
@@ -1,9 +1,18 @@
 class NewrelicMetric < Metric
-  def fetch_value(start=Time.now.utc-20.minutes, finish=Time.now.utc-10.minutes)
-    Newrelic.get_json("https://api.newrelic.com/api/v1/agents/#{host.id}/data.json?metrics[]=#{name}&field=#{field}&summary=1&begin=2013-02-20T00:00:00Z&end=2013-02-21T00:00:00Z")
+  def fetch_value(start, finish)
+    Newrelic.get_json("https://api.newrelic.com/api/v1/agents/#{host.id}/data.json?metrics[]=#{name}&field=#{field}&summary=1&begin=#{start.iso8601(0)}&end=#{finish.iso8601(0)}")
   end
 
-  def generate_sample
-    
+  def generate_sample(start=Time.now.utc-20.minutes, finish=Time.now.utc-10.minutes)
+    raw = fetch_value(start, finish)
+    if raw.nil? || raw[0].empty?
+      puts "Error generating sample for #{host}:#{name}"
+      return
+    end
+    value = raw[0][field]
+    if maximum
+      value /= maximum
+    end
+    self.samples.create(:value => value)
   end
 end
