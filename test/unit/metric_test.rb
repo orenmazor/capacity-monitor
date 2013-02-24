f2a2dfa8c90fcc65dd2e829f33979626c85e12cb
Camilo Lopez <camilo@camilolopez.com>
2013-02-24 12:07:20 -0500

    Fix test

--- test/unit/metric_test.rb
+++ test/unit/metric_test.rb
@@ -2,9 +2,11 @@ require 'test_helper'
 
 class MetricTest < ActiveSupport::TestCase
   test "Metrics createable" do
+    host = Host.new
+
     metric = Metric.new
     metric.name = "Disk IO"
-    metric.host = "App1"
+    metric.host = host
     metric.source = "NewRelic"
     metric.reference = "http://asdf.com"
     metric.maximum = 100
