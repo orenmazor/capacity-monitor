130157057b884fd5f2482b2629d2357e66503a34
Tom Burns <tom.burns@jadedpixel.com>
2013-02-24 10:15:50 -0500

    Add our helper for NewRelic API

--- app/models/newrelic_metric.rb
+++ app/models/newrelic_metric.rb
@@ -1,3 +1,2 @@
 class NewrelicMetric < Metric
-
 end
---
+++ lib/newrelic.rb
@@ -0,0 +1,33 @@
+class Newrelic
+  class < self
+    def account
+      @@account ||= NewRelicApi::Account.first
+    end
+
+    attr_accessible :application_name
+
+    def application
+      @@application ||= account.applications.detect { |a| a.name == @@application_name }
+    end
+
+    def nr_get(url)
+      response = Curl::Easy.perform(url) do |curl|
+        curl.headers["x-api-key"] = NewRelicApi.api_key
+      end
+      response.body_str
+    end
+
+    def get_json(url)
+      response = nr_get(url)
+      JSON.parse(response) unless response.blank?
+    end
+
+    def get_servers(app)
+      get_json("#{app.servers_url}.json")
+    end
+
+    def get_metrics(agent_id)
+      get_json("https://api.newrelic.com/api/v1/agents/#{agent_id}/metrics.json")
+    end
+  end
+end
