bdb8c4e8bf4e72395b32c01576477bb100753a9d
Camilo Lopez <camilo@camilolopez.com>
2013-02-24 17:48:13 -0500

    MOAR url encode

--- app/models/newrelic_metric.rb
+++ app/models/newrelic_metric.rb
@@ -1,11 +1,13 @@
 class NewrelicMetric < Metric
   def fetch_value(start, finish)
+    name = URI.encode(self.name)
     Newrelic.get_json("https://api.newrelic.com/api/v1/agents/#{agent.id}/data.json?metrics[]=#{name}&field=#{field}&summary=1&begin=#{start.iso8601(0)}&end=#{finish.iso8601(0)}")
   end
 
   def generate_sample(start=Time.now.utc-20.minutes, finish=Time.now.utc-10.minutes)
     raw = fetch_value(start, finish)
     if raw.nil? || raw[0].empty?
+      puts name
       puts "Error generating sample for #{agent}:#{name}"
       return
     end
