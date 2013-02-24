52628e5c511d2ac73c2b5404362d75a19c6241c3
Tom Burns <tom.burns@jadedpixel.com>
2013-02-24 11:21:26 -0500

    Also pull name of newrelic app from ENV

--- config/initializers/newrelic.rb
+++ config/initializers/newrelic.rb
@@ -1,1 +1,5 @@
 raise "Please set ENV['NEWRELIC_API_KEY']" unless ENV['NEWRELIC_API_KEY']
+raise "Please set ENV['NEWRELIC_APP']" unless ENV['NEWRELIC_APP']
+
+Newrelic.application_name = ENV['NEWRELIC_APP']
+NewRelicApi.api_key = ENV['NEWRELIC_API_KEY']
