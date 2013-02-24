9af21b6fb349fc10d417d339d5d709e9e916092a
Tom Burns <tom.burns@jadedpixel.com>
2013-02-24 16:25:44 -0500

    generate samples from metrics

--- app/models/newrelic_metric.rb
+++ app/models/newrelic_metric.rb
@@ -1,5 +1,9 @@
 class NewrelicMetric < Metric
   def fetch_value(start=Time.now.utc-20.minutes, finish=Time.now.utc-10.minutes)
-    Newrelic.get_json("https://api.newrelic.com/api/v1/agents/#{host.id}/data.json?metrics[]=#{name}&field=#{field}&summary=1&begin=2013-02-15T00:00:00Z&end=2013-02-16T00:00:00Z")
+    Newrelic.get_json("https://api.newrelic.com/api/v1/agents/#{host.id}/data.json?metrics[]=#{name}&field=#{field}&summary=1&begin=2013-02-20T00:00:00Z&end=2013-02-21T00:00:00Z")
+  end
+
+  def generate_sample
+    
   end
 end
