a9174b1cbc759b232bdfbe6faaaebbb5b45ff6ca
Tom Burns <tom.burns@jadedpixel.com>
2013-02-26 15:10:08 -0500

    refactor to prep for mass API calls

--- app/models/newrelic_metric.rb
+++ app/models/newrelic_metric.rb
@@ -1,16 +1,20 @@
 class NewrelicMetric < Metric
 
-  def generate_sample(start=Time.now.utc-20.minutes, finish=Time.now.utc-10.minutes)
-    raw = Newrelic.get_value(agent.agent_id, name, start, finish)
-    if raw.nil? || raw[0].empty?
-      puts "Error generating sample for #{agent.agent_id}:#{name}"
-      return
-    end
+  def populate(raw)
     value = raw[0][field]
     if maximum
       value /= maximum
     end
 
     self.samples.create(:value => value, :fetched_at => start)
+  end
+
+  def generate_sample(start=Time.now.utc-20.minutes, finish=Time.now.utc-10.minutes)
+    raw = Newrelic.get_value(agent.agent_id, name, start, finish)
+    if raw.nil? || raw[0].empty?
+      puts "Error generating sample for #{agent.agent_id}:#{name} - raw #{raw}"
+      return
+    end
+    populate(raw)
   end
 end
