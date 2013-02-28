6da5e605d9f0129953456ccd5ce156cfe07c4cf7
Tom Burns <tom.burns@jadedpixel.com>
2013-02-28 15:51:32 -0500

    make test mocks & real life live in harmony

--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,17 +3,19 @@ require File.expand_path('../../config/environment', __FILE__)
 require 'rails/test_help'
 require 'mocha/setup'
 
+class Newrelic
+  def application
+    stub(:id => 123)
+  end
+  def account
+    stub(:id => 456)
+  end
+end
+
 class ActiveSupport::TestCase
   # Setup all fixtures in test/fixtures/*.(yml|csv) for all tests in alphabetical order.
   #
   # Note: You'll currently still have to declare fixtures explicitly in integration tests
   # -- they do not yet inherit this setting
   fixtures :all
-
-  def mock_newrelic
-    Newrelic.application = stub(:id => 1234)
-    Newrelic.account = stub(:id => 456)
-  end
-
-  setup :mock_newrelic
 end
