2adc926b48e0cba2eb5aaeed7a9fbafb449ac9a9
Tom Burns <tom.burns@jadedpixel.com>
2013-03-03 02:01:34 -0500

    Expose data as JSON endpoint and pull from HTML

--- app/controllers/capacity_controller.rb
+++ app/controllers/capacity_controller.rb
@@ -1,9 +1,27 @@
 class CapacityController < ApplicationController
-  attr_accessor :metrics
+
+  respond_to :html, :json
 
   def index
+  end
+
+  def data
     @metrics = Metric.where("prediction > 0 AND slope > 0").order("prediction ASC")
     @metrics.reject! { |m| !m.relevant? }
+
+    count = -1
+    @metrics = @metrics.map do |m|
+      count += 1
+      {
+        :name => m.name,
+        :host => m.agent.hostname,
+        :prediction => m.prediction.to_i,
+        :points => JSON.parse(m.points),
+        :best_fit => JSON.parse(m.best_fit),
+        :index => count
+      }
+    end
+    respond_with [{}] + @metrics
   end
 
   protected
