ebfda0f1f116f371e122e482fe64b796286a3168
Tom Burns <tom.burns@jadedpixel.com>
2013-03-05 15:32:58 -0500

    Filter irrelevantly low samples & outliers

--- app/models/metric.rb
+++ app/models/metric.rb
@@ -1,16 +1,30 @@
 require 'matrix'
 
 class Metric < ActiveRecord::Base
-  RELEVANCE_THRESHOLD = 2
+  RELEVANCE_PCT_THRESHOLD = 4
+  OUTLIER_STD = 1
+  IRRELEVANT = 6
 
   belongs_to :agent
   attr_accessor :slope, :offset, :points, :best_fit, :prediction
 
-  has_many :metric_samples
+  has_many :metric_samples, :dependent => :destroy
   alias :samples :metric_samples
 
   def reverse(points)
     points.map { |point| point.reverse }
+  end
+
+  def filter_samples(x, y, &block)
+    index = x.length-1
+    x.reverse_each do |v|
+      if yield(v, y[index])
+        x.delete_at(index)
+        y.delete_at(index)
+      end
+      index -= 1
+    end
+    [x, y]
   end
 
   def generate_points(fact_samples)
@@ -22,14 +36,14 @@ class Metric < ActiveRecord::Base
       x << metric_samples.detect { |s| s.run_id == run }.try(:value)
     end
 
-    index = x.length-1
-    x.reverse_each do |v|
-      if v.nil?
-        x.delete_at(index)
-        y.delete_at(index)
-      end
-      index -= 1
-    end
+    x,y = filter_samples(x, y) { |v, w| v.nil? }
+
+    stddev = x.standard_deviation
+    mean = x.mean
+    min = mean - (stddev*OUTLIER_STD)
+    max = mean + (stddev*OUTLIER_STD)
+
+    x,y = filter_samples(x, y) { |v, w| v < min || v > max || v < IRRELEVANT }
 
     x.map! { |v| v.to_f }
     [x, y]
@@ -61,7 +75,7 @@ class Metric < ActiveRecord::Base
     else
       sql = "SELECT MAX(value), MIN(value) FROM samples WHERE metric_id = #{id}"
       max, min = ActiveRecord::Base.connection.select_rows(sql).first
-      self.relevant = (max.to_f-min.to_f > RELEVANCE_THRESHOLD)
+      self.relevant = (max.to_f-min.to_f > RELEVANCE_PCT_THRESHOLD)
     end
   end
 
