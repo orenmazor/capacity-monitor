f05d66fb47ed657212e8f38e931851b584cd6780
Tom Burns <tom.burns@jadedpixel.com>
2013-04-11 12:35:31 -0400

    only normalize value once

--- app/models/metric_sample.rb
+++ app/models/metric_sample.rb
@@ -1,7 +1,7 @@
 class MetricSample < Sample
   belongs_to :metric
 
-  before_save :normalize_value
+  before_create :normalize_value
   def normalize_value
     self.value = metric.maximum ? ((value.to_f / metric.maximum.to_f) * 100) : value
   end
